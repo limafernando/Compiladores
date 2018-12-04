a7faca608b83deda936a35ee2285396f198da9b6
Luiz Fernando de Lima <luizfernandofpl@gmail.com>
2018-12-04 16:40:30 -0300

    Leitura e carregamento em memória das palavras reservadas

--- compiladorIO.py
+++ compiladorIO.py
@@ -8,6 +8,22 @@ def lePrograma():
     arquivo = open('programaTeste', 'r')
     conteudoArquivo = arquivo.readlines()
     arquivo.close()
+    
+    return conteudoArquivo
+
+def lePalavrasReservadas():
+    """
+    Função para carregamento de palavras reservadas
+    Retorna lista com de palavras reservadas, cada elemento da lista
+    é uma palavra reservada
+    """
+    
+    arquivo = open('palavrasReservadas', 'r')
+    conteudoArquivo = arquivo.read()
+    arquivo.close()
+    conteudoArquivo = conteudoArquivo.split(' ')
+    conteudoArquivo[-1] = conteudoArquivo[-1][:-1] #para apagar \n do último elemento
+
     return conteudoArquivo
 
 def salvaTokes():
--- lexico.py
+++ lexico.py
@@ -2,7 +2,9 @@ import compiladorIO as cio
 
 def main():
     programa = cio.lePrograma()
-    print(programa)
+    #print(programa)
+
+    reservadas = cio.lePalavrasReservadas()
 
 if __name__ == '__main__':
     main()
