d4e48e7bd73f146eabd38b9d00be3c8b6a157761
Luiz Fernando de Lima <luizfernandofpl@gmail.com>
2018-12-04 17:17:13 -0300

    Add importação do módulo e classe token Add da descrição da função main Add do algoritmo inicial da função do autômato

--- lexico.py
+++ lexico.py
@@ -1,10 +1,33 @@
 import compiladorIO as cio
+from token import token
 
 def main():
+    """
+    Função principal do analisador léxico
+    Controla as chamadas das funções utilizadas pelo analisador
+    """
+
     programa = cio.lePrograma()
-    #print(programa)
+    print(programa)
 
     reservadas = cio.lePalavrasReservadas()
+
+    #classificaoTokens = automato(programa)
+    #cio.salvaTokens(classificaoTokens)
+
+def automato(programa):
+    """
+    Função que representa o autômato
+    Retorna lista de classificação de tokens, 
+    cada elemento da lista representa um objeto token
+    """
+    
+    #começa uma lista vazia de objetos token
+    #faz o loop do automato
+    #loop finalizado -> instancia novo objeto token e add na lista de tokens
+    #cria lista de classificação de tokens
+    #loop na lista de tokens """token.getTokenInfo()""" adicionando na lista de classificação
+    #retorna a lista de classificação
 
 if __name__ == '__main__':
     main()
