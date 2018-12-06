a552b7530faa66e0983eca10a948208598c77b1f
Luiz Fernando de Lima <luizfernandofpl@gmail.com>
2018-12-06 10:57:57 -0300

    Identificando floats

--- lexico.py
+++ lexico.py
@@ -27,8 +27,9 @@ def automato(programa):
     cada elemento da lista representa um objeto token
     """
 
-    numeros = ['1','2','3','4','5','6','7','8','9'] #para fazer a checagem
+    numeros = ['0', '1','2','3','4','5','6','7','8','9'] #para fazer a checagem
     classificacao = '' #variavel para guardar a classifição
+    sIdentificador = '' #variavel para guardar a string identificadora
 
     indiceParada = -1 #variavel para guardar o proximo indice a ser analisado
 
@@ -42,6 +43,8 @@ def automato(programa):
     tokens = []
 
     for ele in programa:
+
+        print('\n')
         
         tam = len(ele)
         
@@ -52,38 +55,56 @@ def automato(programa):
             
             else:
                 print('here')
+                
                 if ele[i] in numeros: #recebeu numero
-                    print('here')
+                    print('é numero')
+                    
                     classificacao = 'inteiro'
 
                     for j in range (i+1, tam):
                         
+                        print(ele[j])
                         if ele[j] in numeros:
+                            print('continua inteiro')
+                            
                             classificacao = 'inteiro'
                             indiceParada = j
-                        
+                            
                         elif ele[j] == '.':
-                            classificacao = 'float'
-                            print('float')
+                            print('é float')
 
+                            classificacao = 'float'
+                            
                             for z in range (j+1, tam):
-                                
+                                print('tem casa decimal?')
+
                                 if ele[z] in numeros:
+                                    print('casa decimal')
+
                                     classificacao = 'float'
 
                                 else:
+                                    print('acabaram as casas decimais')
+                                    
                                     indiceParada = z
                                     break
+                            
+                            print('fim de float')
 
-                            tkn = token('af', classificacao, '1')
+                            sIdentificador = ele[i:z]
+                            tkn = token(sIdentificador, classificacao, '1')
                             tokens.append(tkn) 
                             
                             break
 
                         else:
-                            tkn = token('af', classificacao, '1')
+                            print('fim de inteiro')
+
+                            sIdentificador = ele[i:j]
+                            tkn = token(sIdentificador, classificacao, '1')
                             tokens.append(tkn)
-                            print(tkn)
+                            
+                            #print(tkn)
                             
                             break
 
